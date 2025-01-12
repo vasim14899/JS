60bcc990655372c25fe25a119d996b5e83bc6678
VASIM CHOUDHARY <60565419+vasim14899@users.noreply.github.com>
2025-01-12 01:36:59 +0000

    data types

---
+++ 01_basics/variable.js
@@ -0,0 +1,14 @@
+const accountId = 144553
+let accountEmail = "hitesh@google.com"
+var accountPassword = "12345"
+accountCity = "Jaipur"
+let accountState;
+
+// accountId = 2
+
+
+accountEmail = "hc@hc.com"
+accountPassword = 212111
+accountCity = "Mumbai"
+console.log(accountId);
+console.table([accountEmail, accountPassword, accountCity]);
